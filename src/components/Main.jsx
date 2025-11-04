f075c1be1c2e24b815026dd1d42936f6dc744e37
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-04 16:10:37 +0100

    add section (content) to main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,12 @@ export default function Main() {
 
     return (
         <main>
+            <section className="content d-flex align-items-center">
+                <div className="container">
+                    <h2>Content goes here</h2>
+                </div>
+            </section>
+
 
         </main>
     )
